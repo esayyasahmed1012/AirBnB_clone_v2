fe3fdafa0ac4f93eb9795f215e57bb5ac89c3696
esayyas ahmed <eayyasahmed1012@gmail.com>
2022-11-12 09:43:43 +0300

    8-cities_by_states.py

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -1,20 +1,24 @@
-<!DOCTYPE html>
-<HTML lang="en">
-    <HEAD>
-         <TITLE>HBNB</TITLE>
-    </HEAD>
-    <BODY>
-    <H1>States</H1>
-    <UL>
-       {% for state in states.values()|sort(attribute='name') %}
-       <LI>{{ state.id }}: <B>{{ state.name }}</B>
-          <UL>
-             {% for city in state.cities|sort(attribute='name') %}
-                 <LI>{{ city.id }}: <B>{{ city.name }}</B></LI>
-             {% endfor %}
-          </UL>
-        </LI>
-         {% endfor %}
-       </UL>
-    </BODY>
-</HTML>
+#!/usr/bin/python3
+"""Flask app to generate html list of all states from storage"""
+from flask import Flask, render_template
+from models import storage
+app = Flask('web_flask')
+app.url_map.strict_slashes = False
+
+
+@app.route('/cities_by_states')
+def list_of_states():
+    """Render as html alphabetical list of states and cities in each state
+    from `storage`"""
+    states = storage.all('State')
+    return render_template('8-cities_by_states.html', states=states)
+
+
+@app.teardown_appcontext
+def teardown_db(*args, **kwargs):
+    """Close database or file storage"""
+    storage.close()
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
